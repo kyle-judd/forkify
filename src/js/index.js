2b015f2f85257b420fc3789ab4d25561371002a8
kyle-judd <juddkyle1408@gmail.com>
2020-08-27 00:48:28 -0400

    added axios and made first api call

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,14 @@
-// Global app controller
-export default 'I am '
+import axios from 'axios';
+
+async function getResults(query) {
+    try {
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    } catch (error) {
+        alert(error);
+    }
+}
+    
+
+getResults('pizza');
