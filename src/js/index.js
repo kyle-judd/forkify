d8ac7ee1139db50ded142a6eb199687d43926453
kyle-judd <juddkyle1408@gmail.com>
2020-08-27 22:25:26 -0400

    added persistance to likes

--- src/js/index.js
+++ src/js/index.js
@@ -118,9 +118,6 @@ const controlList = () => {
     });
 };
 
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-
 const controlLike = () => {
     if (!state.likes) state.likes = new Likes();
     const currentID = state.recipe.id;
@@ -165,6 +162,20 @@ elements.shopping.addEventListener('click', e => {
         const val = parseFloat(e.target.value, 10);
         state.list.updateCount(id, val);
     }
+});
+
+// Restore liked recipes on page load
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    // Restore likes
+    state.likes.readStorage();
+
+    // Toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // Render existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
 });
 
 // Handling recipe button clicks
